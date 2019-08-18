3618aa41083e82decbe5a3e35a5ff329b8bae48d
Nenad Pantelic <nenadpantelickg@gmail.com>
2019-08-18 22:15:07 +0200

    Add some updates in plan

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import json
 from services.tts_service import  *
 from services.stt_service import *
 from utils.utils import  *
+from config.constants import  *
 langs = None
-INIT_MESSAGE = "Hi, I'm Lindo voice assistant. Choose operating language. Default option is English(USA)!"
+#INIT_MESSAGE = "Hi, I'm Lindo voice assistant. Choose operating language. Default option is English(USA)!"
 
 speaker = Speaker()
 srec = SpeechRecognizer(language="en-US")
