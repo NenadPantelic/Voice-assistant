1a3fa2d7a4e12443846614129274f4ad1fc9f05a
Nenad <nenadpantelickg@gmail.com>
2019-09-26 21:46:17 +0200

    Debug, refactor, test and document imdb service

---
+++ tests/imdb_service_test.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("..")
+from services.webapi.imdb_service import IMDBService
+imdb = IMDBService()
+print(imdb.get_top_movies(num=10).get_result())
+print(imdb.get_movie_details("Don Corleone").get_result())
+print(imdb.get_movie_details("Deer hunter").get_result())
+print(imdb.get_movie_details("Tesna koža").get_result())
+
