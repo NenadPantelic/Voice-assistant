61a5ff5508c42a064767194f54e9fecb2bb60b85
Nenad <nenadpantelickg@gmail.com>
2019-10-06 21:25:50 +0200

    Format arduino code

--- arudino_control/arudino_control.ino
+++ arudino_control/arudino_control.ino
@@ -4,34 +4,27 @@ void setup()
 
 {
 
-
-Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(RELAY1, OUTPUT);
 
 }
 
-  void loop()
+void loop()
 
 {
 
-
-if (Serial.available()) {
-        char serialListener = Serial.read();
-       //int serialListener = Serial.read();
-        Serial.println(serialListener);
-        if (serialListener == '0') {
-  digitalWrite(RELAY1,0);           // Turns ON Relays 1
-   Serial.println("Light ON");
-   //delay(5000);                                      // Wait 2 seconds
-        }
-        else if (serialListener == '1') {
- digitalWrite(RELAY1,1);          // Turns Relay Off
-   Serial.println("Light OFF");
-   //delay(5000);       
-   }
+  if (Serial.available()) {
+    char serialListener = Serial.read();
+    Serial.println(serialListener);
+    if (serialListener == '0') {
+      digitalWrite(RELAY1, 0);
+      Serial.println("Light OFF");
     }
-
-
+    else if (serialListener == '1') {
+      digitalWrite(RELAY1, 1);
+      Serial.println("Light ON");
+    }
+  }
 
 
 }
